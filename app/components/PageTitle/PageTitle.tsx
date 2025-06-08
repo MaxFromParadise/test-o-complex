355689a92877dd0c1dd16083e3bf2773faf0554e
Maxim Polanov <maxdemented372@gmail.com>
2025-06-08 18:25:52 +0300

    created PageTitle component

--- app/components/PageTitle/PageTitle.tsx
+++ app/components/PageTitle/PageTitle.tsx
@@ -1,21 +1,26 @@
+import clsx from 'clsx';
 import { HTMLAttributes, ReactNode } from 'react';
 import styles from './PageTitle.module.scss';
+
 /**
  * PageTitle component
  * Renders an <h1> element with predefined styling and supports standard h1 props.
  **/
-interface Props extends HTMLAttributes<HTMLHeadingElement> {
+
+interface PageTitleProps extends HTMLAttributes<HTMLHeadingElement> {
 	children: ReactNode;
 }
-const PageTitle = ({ children, ...rest }: Props) => {
+const PageTitle = ({ children, className, ...rest }: PageTitleProps) => {
 	return (
 		<h1
 			{...rest}
-			className={`${styles.title}${rest.className ? ' ' + rest.className : ''}`}
+			className={clsx(styles.title, className)}
 		>
 			{children}
 		</h1>
 	);
 };
+
+PageTitle.displayName = 'PageTitle';
 
 export default PageTitle;
